8d59facd9b625fe05be4eddfb8827966d10d3bc6
zain <964641075@qq.com>
2022-11-02 16:08:01 +0800

    bos 新增切分后视频入库

--- cmd/sync_cdc_bos/main.go
+++ cmd/sync_cdc_bos/main.go
@@ -10,15 +10,71 @@ import (
 	"sync"
 	"time"
 	"zeek/toolkit/bos/images"
+	"zeek/toolkit/bos/videos"
 )
 
 func main() {
 
 	batchId := time.Now().Format("200601021504")
-
 	bucket := os.Args[1]
 	prefix := os.Args[2]
+	//loadImages(batchId, bucket, prefix)
+	loadVideos(batchId, bucket, prefix)
+}
 
+func loadVideos(batchId string, bucket string, prefix string) {
+	// 获取 bos 链接
+	client, err := videos.NewClient(
+		os.Getenv("BOS_ENDPOINT"),
+		os.Getenv("BOS_AK"),
+		os.Getenv("BOS_SK"),
+		bucket,
+	)
+	if err != nil {
+		log.Fatal("get bos client err" + err.Error())
+		return
+	}
+
+	list, err := client.List(prefix)
+	if err != nil {
+		log.Fatal("list err" + err.Error())
+		return
+	}
+
+	// 初始化数据库连接
+	db, err := InitDB("cdc")
+	if err != nil {
+		log.Fatalf("init db has err: %s\n", err)
+		return
+	}
+	// bos数据入库
+	for _, objType := range list.Contents {
+		meta, err := client.GetMeta(objType.Key)
+		if err != nil {
+			log.Fatal("get meta err" + err.Error())
+			return
+		}
+
+		content, err := client.GetContent(objType.Key)
+		if err != nil {
+			log.Fatal("1 get err" + err.Error())
+			return
+		}
+
+		db.Create(&videos.TableTestCdcVideos{
+			TaskBatchId: batchId,
+			BosEndpoint: os.Getenv("BOS_ENDPOINT"),
+			BosBucket:   bucket,
+			BosKey:      objType.Key,
+			Md5:         meta.ContentMD5,
+			Length:      meta.ContentLength,
+			Content:     base64.StdEncoding.EncodeToString(content),
+		}).Commit()
+	}
+
+}
+
+func loadImages(batchId string, bucket string, prefix string) {
 	// 获取图片 bos 链接
 	client, err := images.NewImageClient(
 		os.Getenv("BOS_ENDPOINT"),
@@ -119,7 +175,6 @@ func main() {
 	}
 
 	wg.Wait()
-
 }
 
 func InitDB(dbName string) (*gorm.DB, error) {
